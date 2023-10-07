ac5805bfad09ac8184e97dedfb8e54fc6e013085
Ryan Quinn <ryan@bottery.co>
2023-10-07 14:39:08 -0500

    test

---
+++ test.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import time
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def send_message():
+    while True:
+        time.sleep(5)
+        socketio.emit('message', {'data': 'Hello, client!'})
+
+if __name__ == '__main__':
+    socketio.start_background_task(send_message)
+    socketio.run(app, host='0.0.0.0', port=5000)
